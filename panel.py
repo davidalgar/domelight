2a34da7ab20c450e0920ca8fb5c639746f28c6a0
David Algar <david.algar@mindbodyonline.com>
2015-08-05 21:32:50 -0700

    additional work

--- panel.py
+++ panel.py
@@ -1,10 +1,12 @@
 # 18.6 LEDs per 2ft
 #
-#   (0,40)          (40,40)
+#   (0,40)                      (40,40)
 #
 #
+#               (20,20)
 #
-#   (0,0)           (40,0)
+#
+#   (0,0)                       (40,0)
 #
 
 top_start = [0, 40]
@@ -18,17 +20,26 @@ bottom_step_two = [20, 0]
 lines = []
 
 def makepanel():
+    print '1 ' + str(len(lines))
     movestep(top_start, top_step_one)
+    print '2 ' + str(len(lines))
     movestep(top_step_one, top_step_two)
 
+    print '3 ' + str(len(lines))
     movestep(bottom_start, bottom_step_one)
+    print '4 ' + str(len(lines))
     movestep(bottom_step_one, bottom_step_two)
+    print '5 ' +  str(len(lines))
 
 def movestep(start, step_one):
     if start[0] != step_one[0]:
+        print 'a'
         movex(start[0], step_one[0], start[1])
     elif start[1] != step_one[1]:
+        print 'b'
         movey(start[1], step_one[1], start[0])
+    else:
+        print 'c'
 
 def movex(start, end, y):
     for x in range(start, end):
