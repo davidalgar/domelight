e8d250f80d1aac4c47bc43a69131b574d2e25e2e
David Algar <david.algar@mindbodyonline.com>
2015-08-05 21:50:29 -0700

    remove prints

--- panel.py
+++ panel.py
@@ -20,44 +20,31 @@ bottom_step_two = [20, 0]
 lines = []
 
 def makepanel():
-    print '1 ' + str(len(lines))
     movestep(top_start, top_step_one)
-    print '2 ' + str(len(lines))
     movestep(top_step_one, top_step_two)
 
-    print '3 ' + str(len(lines))
     movestep(bottom_start, bottom_step_one)
-    print '4 ' + str(len(lines))
     movestep(bottom_step_one, bottom_step_two)
-    print '5 ' +  str(len(lines))
     lines.append(bottom_step_two)
 
 def movestep(start, step_one):
     if start[0] != step_one[0]:
-        print 'x'
         movex(start[0], step_one[0], start[1])
     elif start[1] != step_one[1]:
-        print 'y'
         movey(start[1], step_one[1], start[0])
-    else:
-        print 'c'
 
 def movex(start, end, y):
-    print 'x.start: ' + str(start) + '  end: '+ str(end)
     step = 1
     if(start>end):
         step = -1
     for x in range(start, end, step):
-        print 'x: '+str(x)
         lines.append([x, y])
 
 def movey(start, end, x):
-    print 'y.start: ' + str(start) + '  end: '+ str(end)
     step = 1
     if(start>end):
         step = -1
     for y in range(start, end, step):
-        print 'y: '+str(y)
         lines.append([x, y])
 
 makepanel()
