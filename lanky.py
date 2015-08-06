928816a9495203b9aeea1206fe4224ffe1501752
David Algar <david.algar@mindbodyonline.com>
2015-08-05 22:49:34 -0700

    lanky pt 1

--- lanky.py
+++ lanky.py
@@ -1,16 +1,16 @@
-import panel.py
+import panel
 
 one = []
-one[0] = [0,0]
-one[1] = [0,20]
-one[2] = [20,20]
-one[3] = [20,0]
+one.append([0,0])
+one.append([0,20])
+one.append([20,20])
+one.append([20,0])
 
 two = []
-two[0] = [20,40]
-two[1] = [20,20]
-two[2] = [40,20]
-two[3] = [40,40]
+two.append([20,40])
+two.append([20,20])
+two.append([40,20])
+two.append([40,40])
 
 str_lines = []
 str_lines += panel.movesteps(one)
