a00f82eb20ed884ba514d0ee7d188af4fa398fc8
david.algar <david@algar.me>
2015-08-06 21:13:30 -0700

    Add rotation function to panel

--- lanky.py
+++ lanky.py
@@ -2,24 +2,18 @@ import panel
 
 adjustment = 40
 
+
 def adjust(list):
     result = []
     for i in list:
-        i = [x+adjustment for x in i]
+        i = [x + adjustment for x in i]
         result.append(i)
     return result
 
-one = []
-one.append([0,0])
-one.append([0,20])
-one.append([20,20])
-one.append([20,0])
 
-two = []
-two.append([20,40])
-two.append([20,20])
-two.append([40,20])
-two.append([40,40])
+one = [[0, 0], [0, 20], [20, 20], [20, 0]]
+
+two = [[20, 40], [20, 20], [40, 20], [40, 40]]
 
 str_lines = []
 str_lines += panel.movesteps(adjust(one))
@@ -30,7 +24,5 @@ str_lines += panel.movesteps(adjust(panel.translate_y(one)))
 str_lines += panel.movesteps(adjust(panel.translate_y(two)))
 str_lines += panel.movesteps(adjust(panel.translate_x(panel.translate_y(one))))
 str_lines += panel.movesteps(adjust(panel.translate_y(panel.translate_x(two))))
-
-
 
 print '[\n' + ',\n'.join(str_lines) + '\n]'
--- panel.py
+++ panel.py
@@ -25,17 +25,18 @@ def makepanel():
 
     print '[\n' + ',\n'.join(lines) + '\n]'
 
+
 def movesteps(steps):
     lines = []
-    for i in range(len(steps)-1):
+    for i in range(len(steps) - 1):
         step = steps[i]
-        next = steps[i+1]
+        next = steps[i + 1]
         lines += movestep(step, next)
-    lines.append(steps[len(steps)-1])
+    lines.append(steps[len(steps) - 1])
 
     str_lines = []
     for point in lines:
-        str_lines.append('  {"point": [%.2f, %.2f, %.2f]}' % (point[0]/20.0, point[1]/20.0, 0))
+        str_lines.append('  {"point": [%.2f, %.2f, %.2f]}' % (point[0] / 20.0, point[1] / 20.0, 0))
 
     return str_lines
 
@@ -48,34 +49,48 @@ def movestep(start, step_one):
         lines += movey(start[1], step_one[1], start[0])
     return lines
 
+
 def movex(start, end, y):
     lines = []
     step = 1
-    if(start>end):
+    if (start > end):
         step = -1
     for x in range(start, end, step):
         lines.append([x, y])
     return lines
 
+
 def movey(start, end, x):
     lines = []
     step = 1
-    if(start>end):
+    if (start > end):
         step = -1
     for y in range(start, end, step):
         lines.append([x, y])
     return lines
 
-#makepanel()
+
+# makepanel()
 
 def translate_x(strip):
     new_strip = []
     for point in strip:
-        new_strip.append([point[0]*-1, point[1]])
+        new_strip.append([point[0] * -1, point[1]])
     return new_strip
+
 
 def translate_y(strip):
     new_strip = []
     for point in strip:
-        new_strip.append([point[0], point[1]*-1])
+        new_strip.append([point[0], point[1] * -1])
+    return new_strip
+
+
+def rotate90(strip, rotationpoint):
+    new_strip = []
+    for point in strip:
+        curpoint = [point[0] - rotationpoint[0], point[1] - rotationpoint[1]]
+        curpoint = [curpoint[1] * -1, curpoint[0]]
+        curpoint = [curpoint[0] - rotationpoint[0], curpoint[1] - rotationpoint[1]]
+        new_strip.append(curpoint)
     return new_strip
